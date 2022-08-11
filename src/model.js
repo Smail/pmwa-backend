419b0f62cb7c69c5f6a7119706dd19013d95c423
Smail <44433524+Smail@users.noreply.github.com>
2022-08-11 15:12:27 +0200

    only allow to push users if their username doesn't already exist

--- src/model.js
+++ src/model.js
@@ -1,6 +1,24 @@
 const { assert } = require('console');
 
 const users = [];
+// Observe push method on users array.
+// Automatically serialize users array.
+users.push = function () {
+  // Check if the elements pushed are of type User.
+  for (const key in arguments) {
+    const value = arguments[key];
+
+    if (!(value instanceof User)) {
+      throw new Error('Element pushed into users array is not of Type User. ' + JSON.stringify(value));
+    }
+
+    if (users.filter(user => user.username.toLowerCase() === value.username.toLowerCase()).length > 0) {
+      throw new Error('Username already exists');
+    }
+  }
+
+  Array.prototype.push.apply(this, arguments);
+};
 
 function getUserFromUsername(username) {
   assert(username, new Error('Username is null'));
