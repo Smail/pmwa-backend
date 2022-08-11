78c00a1ac4ce67af0910da031b8673656874f896
Smail <44433524+Smail@users.noreply.github.com>
2022-08-11 15:10:40 +0200

    only add unique mock accounts

--- src/mock.js
+++ src/mock.js
@@ -5,26 +5,34 @@ const { User } = require('./User');
 require('dotenv').config();
 
 // Add faker.js data if in debug mode
-if (process.env.DEBUG) {  
+if (process.env.DEBUG) {
+  const usersTmp = [];
   (async () => {
     const { faker } = require('@faker-js/faker/locale/en_US');
 
-    async function addFakeData() {
-      faker.seed(123);
+    faker.seed(123);
 
-      for (let i = 0; i < 20; i++) {
-        const randomName = faker.name.findName(); // Jane Doe
-        const firstName = randomName.split(' ')[0]; // Jane
-        const lastName = randomName.split(' ')[1]; // Doe
-        const username = faker.internet.userName(firstName, lastName);
-        const email = faker.internet.email();
-        const password = faker.internet.password(15);
-        users.push(new User(username, firstName, lastName, email, password));
-      }
+    for (let i = 0; i < 20; i++) {
+      const randomName = faker.name.findName(); // Jane Doe
+      const firstName = randomName.split(' ')[0]; // Jane
+      const lastName = randomName.split(' ')[1]; // Doe
+      const username = faker.internet.userName(firstName, lastName);
+      const email = faker.internet.email();
+      const password = faker.internet.password(15);
 
-      users.push(new User('smail', 'Smail', 'Mustermann', 'smail@example.com', 'Smail1234'));
+      usersTmp.push(new User(username, firstName, lastName, email, password));
     }
 
-    addFakeData();
+    usersTmp.push(new User('smail', 'Smail', 'Mustermann', 'smail@example.com', 'Smail1234'));
+    
+    // Add created mock elements to users array
+    for (const user of usersTmp) {
+      const username = user.username.toLowerCase();
+
+      // Only add unique usernames
+      if (users.find(u => u.username.toLowerCase() === username) == null) {
+        users.push(user);
+      }
+    }
   })();
 }
