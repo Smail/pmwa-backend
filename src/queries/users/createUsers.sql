38ecab9a8095e5e10bb31dddc98159fdde7ece82
Smail <44433524+Smail@users.noreply.github.com>
2022-08-11 15:27:08 +0200

    fix: add missing email column

--- src/queries/users/createUsers.sql
+++ src/queries/users/createUsers.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
   username TEXT UNIQUE NOT NULL,
   firstName TEXT NOT NULL,
   lastName TEXT NOT NULL,
+  email TEXT NOT NULL,
   passwordHash TEXT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
