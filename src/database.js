668e111ca8a275e0d2380d1aee9c50a0128308c6
Smail <44433524+Smail@users.noreply.github.com>
2022-08-20 00:23:54 +0200

    feat: automatically close db on exit, SIGTERM, etc.

--- src/database.js
+++ src/database.js
@@ -44,6 +44,11 @@ function loadAllSqlQueries() {
   return queries;
 }
 
+process.on('exit', () => db.close());
+process.on('SIGHUP', () => process.exit(128 + 1));
+process.on('SIGINT', () => process.exit(128 + 2));
+process.on('SIGTERM', () => process.exit(128 + 15));
+
 module.exports = {
   queries: loadAllSqlQueries(),
   db: db,
