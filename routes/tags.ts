c4ef28948dc7dcef4d0cac7cf1dd2d70bd08fa9f
Smail <44433524+Smail@users.noreply.github.com>
2022-08-28 07:54:59 +0200

    feat(routes): add route to return all available tag names

--- routes/tags.ts
+++ routes/tags.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { requireAccessToken, loadAuthenticatedUser } from './auth';
 import { Task } from "../src/Task";
+import { Tag } from "../src/Tag";
 
 const router = express.Router({ mergeParams: true });
 
 router.use(requireAccessToken);
 router.use(loadAuthenticatedUser);
+
+/* GET all available tag names. */
+router.get('/names', function (req, res, next) {
+  res.status(StatusCodes.OK).send(Tag.getAllTagNames());
+});
 
 /* GET task's tags. */
 router.get('/:taskUuid', function (req, res, next) {
