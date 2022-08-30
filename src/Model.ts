f6d885eb47daee799320b9041780c1da8092aebe
Smail <44433524+Smail@users.noreply.github.com>
2022-08-30 20:48:24 +0200

    Refactor model to use repositories

--- src/Model.ts
+++ src/Model.ts
@@ -1,49 +1,17 @@
-import { UserDepreciated } from "@models/User.depreciated";
-import * as Database from "./Database";
+import sqlite3 from "better-sqlite3";
+import { IUserRepository } from "@models/IUserRepository";
+import { UserRepositorySQLite } from "@models/UserRepositorySQLite";
+import { ITasksRepository } from "@models/ITasksRepository";
+import { ITagsRepository } from "@models/ITagsRepository";
+import { IRefreshTokenRepository } from "@models/IRefreshTokenRepository";
+import { TasksRepositorySQLite } from "@models/TasksRepositorySQLite";
+import { TagsRepositorySQLite } from "@models/TagsRepositorySQLite";
+import { RefreshTokenRepositorySQLite } from "@models/RefreshTokenRepositorySQLite";
 
-// Create all tables
-for (const queryName in Database.queries) {
-  if (queryName.toLowerCase().startsWith("createTable".toLowerCase())) {
-    if (!queryName.startsWith("createTable")) {
-      console.warn(`File ${queryName} doesn't match camelCase naming convention.`);
-    }
-    Database.db.prepare(Database.queries[queryName]).run();
-  }
+export class Model {
+  private static readonly db: sqlite3 = sqlite3(process.env.DB_PATH);
+  public static readonly userRepository: IUserRepository = new UserRepositorySQLite(this.db);
+  public static readonly tasksRepository: ITasksRepository = new TasksRepositorySQLite(this.db);
+  public static readonly tagRepository: ITagsRepository = new TagsRepositorySQLite(this.db);
+  public static readonly refreshTokenRepository: IRefreshTokenRepository = new RefreshTokenRepositorySQLite(this.db);
 }
-
-const users: UserDepreciated[] = [];
-// Observe push method on users array.
-// Automatically serialize users array.
-users.push = function () {
-  // Check if the elements pushed are of type User.
-  for (const key in arguments) {
-    const value = arguments[key];
-
-    if (!(value instanceof UserDepreciated)) {
-      throw new Error("Element pushed into users array is not of Type User. " + JSON.stringify(value));
-    }
-
-    if (users.filter(user => user.username.toLowerCase() === value.username.toLowerCase()).length > 0) {
-      throw new Error("Username already exists");
-    }
-  }
-
-  return Array.prototype.push.apply(this, arguments);
-};
-
-function getUserFromUsername(username: string | null): UserDepreciated | undefined {
-  if (!username) return undefined;
-  return users.find(user => user.username.toLowerCase() === username.toLowerCase());
-}
-
-export function getUserFromUuid(uuid: string | null): UserDepreciated | undefined {
-  if (!uuid) return undefined;
-  return users.find(user => user.uuid === uuid);
-}
-
-function existsUsername(username: string | null): boolean {
-  if (!username) return false;
-  return getUserFromUsername(username) != null;
-}
-
-export { users, getUserFromUsername, existsUsername };
