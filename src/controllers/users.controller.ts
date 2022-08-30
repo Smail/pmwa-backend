4bafe5f3dc6754018c7d533670911b2fc1cba7cd
Smail <44433524+Smail@users.noreply.github.com>
2022-08-30 20:50:31 +0200

    refactor: use new model repositories

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,11 @@
 import createError from "http-errors";
 import { StatusCodes } from "http-status-codes";
-import * as Model from "../Model";
-import { UserBuilder, UserDepreciated } from "@models/User.depreciated";
+import { Model } from "../Model";
+import { User } from "@models/User";
+import { v4 as uuidv4 } from "uuid";
 
 export const get_user = (req, res, next) => {
-  // @ts-ignore TODO
-  res.send(Model.getUserFromUuid(req.body.uuid)?.toString()); // TODO this is weird code? toString doesnt exists TODO (...)?.username
+  res.send(Model.userRepository.read(req.uuid).serializeToObject());
 };
 
 export const create_user = async (req, res, next) => {
@@ -16,60 +16,39 @@ export const create_user = async (req, res, next) => {
   }
 
   // TODO regex test for first and last name, i.e., no special characters like '@' in name.
-  if (UserDepreciated.isValidEmail(email)) return next(createError(StatusCodes.BAD_REQUEST, "Invalid email"));
-  if (UserDepreciated.isPasswordWeak(password)) return next(createError(StatusCodes.UNPROCESSABLE_ENTITY, "Password is too weak"));
-  if (Model.existsUsername(username)) return next(createError(StatusCodes.CONFLICT, "Username already exists"));
+  if (!User.isValidEmail(email)) return next(createError(StatusCodes.BAD_REQUEST, "Invalid email"));
+  if (!User.isValidPassword(password)) return next(createError(StatusCodes.UNPROCESSABLE_ENTITY, "Invalid password"));
+  if (Model.userRepository.findUsername(username)) return next(createError(StatusCodes.CONFLICT, "Username already exists"));
 
-  // Create new user
-  const user: UserDepreciated = new UserBuilder()
-    .addUsername(username)
-    .addFirstName(firstName)
-    .addLastName(lastName)
-    .addEmail(email)
-    .addPassword(password)
-    .build();
+  const user = new User();
 
-  Model.users.push(user);
-  res.status(StatusCodes.CREATED).send({ id: user.uuid });
+  user.id = uuidv4();
+  user.username = username;
+  user.firstName = firstName;
+  user.lastName = lastName;
+  user.email = email;
+  user.password = password;
+
+  Model.userRepository.create(user);
+  res.status(StatusCodes.CREATED).send({ id: user.id });
 };
 
 export const update_user = (req, res, next) => {
-  // TODO very inefficient. Use repository
   const { username, firstName, lastName, email, password } = req.body;
+  const user = Model.userRepository.read(req.uuid);
 
-  const user: UserDepreciated = new UserDepreciated(req.uuid);
-  const bUsername = user.username;
-  const bFirstName = user.firstName;
-  const bLastName = user.lastName;
-  const bEmail = user.email;
-  const bPasswordHash = user.passwordHash;
+  // TODO validity checks
+  if (username) user.username = username;
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (email) user.email = email;
+  if (password) user.password = password;
 
-  try {
-    if (username) user.username = username;
-    if (firstName) user.firstName = firstName;
-    if (lastName) user.lastName = lastName;
-    if (email) user.email = email;
-    if (password) user.password = password;
-
-    res.sendStatus(StatusCodes.OK);
-  } catch (error) {
-    // Restore
-    try {
-      user.username = bUsername;
-      user.firstName = bFirstName;
-      user.lastName = bLastName;
-      user.email = bEmail;
-      user.passwordHash = bPasswordHash;
-
-      createError(StatusCodes.BAD_REQUEST, error.message);
-    } catch (error2) {
-      createError(StatusCodes.INTERNAL_SERVER_ERROR,
-        "Could not restore state.\nFirst error: " + error.message + "\nSecond error: " + error2.message);
-    }
-  }
+  Model.userRepository.update(user);
+  res.sendStatus(StatusCodes.OK);
 };
 
 export const get_display_name = (req, res, next) => {
-  // @ts-ignore TODO
-  res.send({ displayName: Model.getUserFromUsername(req.accessTokenContent.username)?.displayName }); // TODO null coalescing
+  const user = Model.userRepository.read(req.uuid);
+  res.status(StatusCodes.OK).send(user.displayName);
 };
