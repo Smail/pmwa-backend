638ba0a183f875323602b787545f76db53b81cfd
Smail <44433524+Smail@users.noreply.github.com>
2022-08-30 09:42:21 +0200

    style: fix styling

--- src/queries/tasks/createTableTasks.sql
+++ src/queries/tasks/createTableTasks.sql
@@ -1,45 +1,9 @@
-CREATE TABLE IF NOT EXISTS tasks
-(
-    uuid
-    TEXT
-    PRIMARY
-    KEY
-    NOT
-    NULL,
-    userUuid
-    TEXT
-    NOT
-    NULL,
-    name
-    TEXT
-    NOT
-    NULL,
-    content
-    TEXT,
-    isDone
-    INT
-    DEFAULT
-    0
-    NOT
-    NULL
-    CHECK
-(
-    isDone
-    ==
-    0
-    OR
-    isDone
-    ==
-    1
-),
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY
-(
-    userUuid
-) REFERENCES users
-(
-    uuid
-) ON UPDATE CASCADE
-  ON DELETE CASCADE
-    )
--- TODO primary key with uuid and useruuid
+CREATE TABLE IF NOT EXISTS tasks (
+    uuid       TEXT PRIMARY KEY NOT NULL,
+    userUuid   TEXT             NOT NULL,
+    name       TEXT             NOT NULL,
+    content    TEXT,
+    isDone     INT                       DEFAULT 0 NOT NULL CHECK ( isDone == 0 OR isDone == 1),
+    created_at TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (userUuid) REFERENCES users (uuid) ON UPDATE CASCADE ON DELETE CASCADE
+)
