c88ff0c5e25ff2728d0a3a1866e294156f8797b6
Smail <44433524+Smail@users.noreply.github.com>
2022-08-30 02:02:44 +0200

    refactor: move middleware function out into new file

---
+++ src/middleware/checkUsername.ts
@@ -0,0 +1,13 @@
+import createError from "http-errors";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+export function checkUsername(req, res, next) {
+  const tokenContent = req.accessTokenContent;
+
+  if (!tokenContent.username) return next(createError(StatusCodes.BAD_REQUEST, 'No username in token'));
+  if (tokenContent.username !== req.params.username) {
+    return next(createError(StatusCodes.FORBIDDEN, ReasonPhrases.FORBIDDEN));
+  }
+
+  next();
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,20 +1,11 @@
 import express from 'express';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { checkUsername } from "@middleware/checkUsername";
 import { requireAccessToken } from '@middleware/auth';
 import createError from "http-errors";
 import { get_display_name, get_user } from "@controllers/usersController";
 
 const router = express.Router();
-
-function checkUsername(req, res, next) {
-  const tokenContent = req.accessTokenContent;
-
-  if (!tokenContent.username) return next(createError(StatusCodes.BAD_REQUEST, 'No username in token'));
-  if (tokenContent.username !== req.params.username) {
-    return next(createError(StatusCodes.FORBIDDEN, ReasonPhrases.FORBIDDEN));
-  }
-  next();
-}
 
 /* GET user */
 router.get('/username/:username', requireAccessToken, checkUsername, get_user);
