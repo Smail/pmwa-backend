949c4c0a55a7387dbd85870d89a80ed0fb029c4b
Smail <44433524+Smail@users.noreply.github.com>
2022-09-03 18:19:29 +0200

    Remove route for creating a user (POST /users/)

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,15 +1,12 @@
 import express from "express";
 import { requireAccessToken } from "@middleware/requireAccessToken";
-import { create_user, get_user, update_user } from "@controllers/users.controller";
+import { get_user, update_user } from "@controllers/users.controller";
 import { requireUuid } from "@middleware/requireUuid";
 
 const router = express.Router();
 
 /* GET user */
 router.get("/:uuid", requireAccessToken, requireUuid, get_user);
-
-/* CREATE user */
-router.post("/", create_user);
 
 /* UPDATE user */
 router.patch("/", requireAccessToken, requireUuid, update_user);
