9a83406e695439a6cf8a6aac75702c940e8868b4
Smail <44433524+Smail@users.noreply.github.com>
2022-09-05 19:07:12 +0200

    fix(controller): error handling didn't work

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -16,13 +16,15 @@ function createUser(req, next): User {
   const { username, firstName, lastName, email, password } = req.body;
 
   for (const key of ["username", "firstName", "lastName", "email", "password"]) {
-    if (!req.body[key]) return next(createError(StatusCodes.BAD_REQUEST, `Missing key '${key}'`));
+    if (!req.body[key]) throw createError(StatusCodes.BAD_REQUEST, `Missing key '${key}'`);
   }
 
   // TODO regex test for first and last name, i.e., no special characters like '@' in name.
   if (!User.isValidEmail(email)) return next(createError(StatusCodes.BAD_REQUEST, "Invalid email"));
   if (!User.isValidPassword(password)) return next(createError(StatusCodes.UNPROCESSABLE_ENTITY, "Invalid password"));
-  if (Model.userRepository.findUsername(username)) return next(createError(StatusCodes.CONFLICT, "Username already exists"));
+  if (Model.userRepository.findUsername(username) != null) {
+    throw createError(StatusCodes.CONFLICT, "Username already exists");
+  }
 
   const user = new User();
 
@@ -39,9 +41,13 @@ function createUser(req, next): User {
 }
 
 export const sign_up_user = async (req, res, next) => {
-  const user = createUser(req, next);
-
-  res.status(StatusCodes.CREATED).send({ userId: user.id, ...createAccessAndRefreshToken(user) });
+  try {
+    const user = createUser(req, next);
+    console.log(user.id);
+    res.status(StatusCodes.CREATED).send({ userId: user.id, ...createAccessAndRefreshToken(user) });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const sign_in_user = async (req, res, next) => {
