971b39c69d530f5f706fb929515c76b1579b0673
Smail <44433524+Smail@users.noreply.github.com>
2022-09-06 23:32:19 +0200

    fix: Remove UUID requirement and remove function since it's unused now

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { refresh_token, sign_in_user, sign_up_user } from "@controllers/auth.controller";
-import { requireUuidInBody } from "@middleware/requireUuidInBody";
 
 const router = express.Router();
 
@@ -11,6 +10,6 @@ router.post("/sign-in", sign_in_user);
 router.post("/sign-up", sign_up_user);
 
 // Issue new access and refresh token with a provided refresh token.
-router.post("/refresh-token", requireUuidInBody, refresh_token);
+router.post("/refresh-token", refresh_token);
 
 export { router };
