92c655b524a787919a4a39904091122f8e3fac51
Smail <44433524+Smail@users.noreply.github.com>
2022-09-06 16:22:04 +0200

    refactor: Apply new db scheme

--- src/models/repositories/sqlite/UserRepositorySQLite.ts
+++ src/models/repositories/sqlite/UserRepositorySQLite.ts
@@ -25,14 +25,14 @@ export class UserRepositorySQLite extends SQLiteTable implements IUserRepository
   }
 
   public create(user: User): void {
-    const query = `INSERT INTO users(userId, username, displayName, firstName, lastName, email, passwordHash)
+    const query = `INSERT INTO Users(userId, username, displayName, firstName, lastName, email, passwordHash)
                    VALUES ($userId, LOWER($username), $displayName, $firstName, $lastName, $email, $passwordHash)`;
     runTransaction(this.db, query, user.serializeToObject());
   }
 
   public read(userId: string): User {
     const query = `SELECT userId, username, displayName, firstName, lastName, email
-                   FROM users
+                   FROM Users
                    WHERE userId = $userId`;
     const row = this.db.prepare(query).get({ userId: userId });
     if (row == null) throw new Error("User not found: no such ID");
@@ -41,12 +41,12 @@ export class UserRepositorySQLite extends SQLiteTable implements IUserRepository
   }
 
   public readAll(): User[] {
-    const query = `SELECT userId, username, displayName, firstName, lastName, email FROM users`;
+    const query = `SELECT userId, username, displayName, firstName, lastName, email FROM Users`;
     return this.db.prepare(query).all().map(row => ISerializable.deserialize(User, row));
   }
 
   public update(user: User): void {
-    const query = `UPDATE users
+    const query = `UPDATE Users
                    SET username     = LOWER($username),
                        displayName  = $displayName,
                        firstName    = $firstName,
@@ -58,19 +58,19 @@ export class UserRepositorySQLite extends SQLiteTable implements IUserRepository
   }
 
   public delete(user: User): void {
-    const query = `DELETE FROM users WHERE userId = $userId`;
+    const query = `DELETE FROM Users WHERE userId = $userId`;
     runTransaction(this.db, query, { userId: user.id });
   }
 
   public findUsername(username: string): User | null {
-    const query = `SELECT userId FROM users WHERE username = LOWER($username)`;
+    const query = `SELECT userId FROM Users WHERE username = LOWER($username)`;
     const row = this.db.prepare(query).get({ username: username });
 
     return (row != null) ? this.read(row.id) : null;
   }
 
   public checkPassword(user: User, password: string, comparePasswords: (password, passwordHash) => boolean): boolean {
-    const query = `SELECT passwordHash FROM users WHERE userId = $userId`;
+    const query = `SELECT passwordHash FROM Users WHERE userId = $userId`;
     const row = this.db.prepare(query).get({ userId: user.id });
     if (row.length === 0) throw new Error("User not found: no such ID");
 
