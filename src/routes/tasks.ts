81067be7532e15d8e354ce611917ef35b86ff2b3
Smail <44433524+Smail@users.noreply.github.com>
2022-09-06 16:01:16 +0200

    refactor: Make tasks route RESTful

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -1,26 +1,27 @@
 import express from "express";
-import { router as tagsRouter } from "@routes/tags";
-import { requireUuidInBody } from "@middleware/requireUuidInBody";
-import { requireTaskName } from "@middleware/requireTaskName";
 import { requireAccessToken } from "@middleware/requireAccessToken";
 import { requireAuthenticatedUser } from "@middleware/requireAuthenticatedUser";
-import { create_task, delete_task, get_tasks, update_task } from "@controllers/task.controller";
+import { create_task, delete_task, get_task, get_tasks, update_task } from "@controllers/task.controller";
+import { router as tagsRouter } from "@routes/tags";
 
 const router = express.Router();
 
 router.use(requireAccessToken);
 router.use(requireAuthenticatedUser);
 
-/* GET user's tasks. */
+/* GET user tasks. */
 router.get("/", get_tasks);
 
-/* POST Create new user task. */
-router.post("/", requireTaskName, create_task);
+/* GET user task. */
+router.get("/:taskId", get_task);
 
-/* Update user task. */
-router.patch("/", requireUuidInBody, update_task);
+/* POST Create new user task. */
+router.post("/", create_task);
+
+/* PATCH Update user task. */
+router.patch("/:taskId", update_task);
 
 /* DELETE user task. */
-router.delete("/", requireUuidInBody, delete_task);
+router.delete("/:taskId", delete_task);
 
 export { router };
