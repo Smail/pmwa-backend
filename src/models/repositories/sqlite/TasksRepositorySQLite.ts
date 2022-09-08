589c34e3575a3f7648f3c463750dae76ea925db2
Smail <44433524+Smail@users.noreply.github.com>
2022-09-08 21:35:35 +0200

    fix: SQLite type error (boolean)

--- src/models/repositories/sqlite/TasksRepositorySQLite.ts
+++ src/models/repositories/sqlite/TasksRepositorySQLite.ts
@@ -4,6 +4,7 @@ import { sqlite3 } from "better-sqlite3";
 import { runTransaction } from "../../../util/db/runTransaction";
 import * as ISerializable from "@models/repositories/ISerializable";
 import { SQLiteTable } from "@models/repositories/sqlite/SQLiteTable";
+import { ITaskRecord } from "@models/ITaskRecord";
 
 export class TasksRepositorySQLite extends SQLiteTable implements ITasksRepository {
   public static readonly tableSchema: string =
@@ -19,22 +20,34 @@ export class TasksRepositorySQLite extends SQLiteTable implements ITasksReposito
     super(db, TasksRepositorySQLite.tableSchema);
   }
 
+  private static convertIsDoneToInt(obj: ITaskRecord) {
+    return { ...obj, isDone: obj.isDone ? 1 : 0 };
+  }
+
+  private static convertIsDoneToBoolean(obj: { taskId: string, name: string, content: string | null, isDone: number }) {
+    if (obj.isDone === 1 || obj.isDone === 0) throw new Error("Invalid argument");
+    return { ...obj, isDone: obj.isDone === 1 };
+  }
+
   public create(task: Task): void {
     const query = `INSERT INTO Tasks(taskId, name, content, isDone)
-                   VALUES ($taskId, $taskName, $taskContent, $isTaskDone)`;
-    runTransaction(this.db, query, task.serializeToObject());
+                   VALUES ($taskId, $name, $content, $isDone)`;
+    runTransaction(this.db, query, TasksRepositorySQLite.convertIsDoneToInt(task.serializeToObject()));
   }
 
   public read(taskId: string): Task | null {
     const query = `SELECT taskId, name, content, isDone FROM Tasks WHERE taskId = $taskId`;
     const row = this.db.prepare(query).get({ taskId: taskId });
 
-    return (row != null) ? ISerializable.deserialize(Task, row) : null;
+    return (row != null) ? ISerializable.deserialize(Task, TasksRepositorySQLite.convertIsDoneToBoolean(row)) : null;
   }
 
   public readAll(): Task[] {
     const query = `SELECT taskId, name, content, isDone FROM Tasks`;
-    return this.db.prepare(query).all().map(row => ISerializable.deserialize(Task, row));
+    return this.db.prepare(query)
+      .all()
+      .map(row => TasksRepositorySQLite.convertIsDoneToBoolean(row))
+      .map(row => ISerializable.deserialize(Task, row));
   }
 
   public update(task: Task): void {
@@ -43,7 +56,7 @@ export class TasksRepositorySQLite extends SQLiteTable implements ITasksReposito
                        content = $content,
                        isDone  = $isDone
                    WHERE taskId = $taskId`;
-    runTransaction(this.db, query, task.serializeToObject());
+    runTransaction(this.db, query, TasksRepositorySQLite.convertIsDoneToInt(task.serializeToObject()));
   }
 
   public delete(task: Task): void {
