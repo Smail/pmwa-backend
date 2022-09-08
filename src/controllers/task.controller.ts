5ec0b2ffd9c4381956d7c7739cadbff1049c3882
Smail <44433524+Smail@users.noreply.github.com>
2022-09-08 21:45:55 +0200

    feat: Check for valid arguments and return appropriate errors

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -38,9 +38,22 @@ export const update_task = (req: { body: ITaskRecord; user: User; params: { task
   const task = userTasksRepository.read(req.params.taskId);
   if (task == null) throw createError(StatusCodes.NOT_FOUND, "Task not found");
 
-  if (name) task.name = name;
-  if (content) task.content = content;
-  if (isDone) task.isDone = isDone;
+  if (!Task.isValidId(req.params.taskId)) {
+    throw createError(StatusCodes.BAD_REQUEST, `Invalid argument: invalid ID taskId = ${req.params.taskId}`);
+  }
+  if (name != null && typeof name !== "string") {
+    throw createError(StatusCodes.BAD_REQUEST, `Invalid type: name is not a string`);
+  }
+  if (content != null && typeof content !== "string") {
+    throw createError(StatusCodes.BAD_REQUEST, `Invalid type: content is not a string`);
+  }
+  if (isDone != null && typeof isDone !== "boolean") {
+    throw createError(StatusCodes.BAD_REQUEST, `Invalid type: isDone is not a boolean`);
+  }
+
+  if (name != null) task.name = name;
+  if (content != null) task.content = content;
+  if (isDone != null) task.isDone = isDone;
 
   Model.tasksRepository.update(task);
   res.sendStatus(StatusCodes.NO_CONTENT);
