ee8861eba5088d2778a2db13737e553998b923ef
Smail <44433524+Smail@users.noreply.github.com>
2022-09-08 21:38:31 +0200

    refactor!: Remove userId form Task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -5,7 +5,6 @@ import { ITaskRecord } from "@models/ITaskRecord";
 
 export class Task implements ISerializable {
   public id: string;
-  public userId: string;
   public name: string;
   public content: string | null;
   public isDone: boolean = false;
@@ -14,9 +13,8 @@ export class Task implements ISerializable {
     return isValidUuid(taskId);
   }
 
-  public static throwIfInvalid({ taskId, userId, name, content, isDone }: ITaskRecord): void {
+  public static throwIfInvalid({ taskId, name, content, isDone }: ITaskRecord): void {
     if (!Task.isValidId(taskId)) throw new Error(`Invalid argument: taskId = ${taskId}`);
-    if (!User.isValidId(userId)) throw new Error(`Invalid argument: userId = ${userId}`);
     if (name == null) throw new Error(`Invalid argument: name is null`);
     if (content != null && typeof content !== "string") throw new Error(`Invalid argument: content is not a string or null`);
     if (isDone == null) throw new Error(`Invalid argument: isDone is null`);
@@ -28,11 +26,10 @@ export class Task implements ISerializable {
   }
 
   // TODO assign only if not null else default value
-  public deserializeFromObject({ taskId, userId, name, content, isDone }: ITaskRecord): void {
-    Task.throwIfInvalid({ taskId, userId, name, content, isDone });
+  public deserializeFromObject({ taskId, name, content, isDone }: ITaskRecord): void {
+    Task.throwIfInvalid({ taskId, name, content, isDone });
     // TODO loop over typescript class object https://stackoverflow.com/a/66645452/9258134
     this.id = taskId;
-    this.userId = userId;
     this.name = name;
     this.content = content;
     this.isDone = isDone;
@@ -41,7 +38,6 @@ export class Task implements ISerializable {
   public serializeToObject(): ITaskRecord {
     const o: ITaskRecord = {
       taskId: this.id,
-      userId: this.userId,
       name: this.name,
       content: this.content,
       isDone: this.isDone,
