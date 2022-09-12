9a5938d96d6bb42185d14032f953240408f36d9f
Smail <44433524+Smail@users.noreply.github.com>
2022-09-12 22:38:19 +0200

    style: Remove new line

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors({ origin: true, credentials: true }));
 
-
 // Add routes
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
