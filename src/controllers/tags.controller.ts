b9a8e820da878b5698974321af2865703f98d12b
Smail <44433524+Smail@users.noreply.github.com>
2022-09-17 02:05:25 +0200

    feat: Check request object; add better errors; add types to parameters

--- src/controllers/tags.controller.ts
+++ src/controllers/tags.controller.ts
@@ -3,8 +3,21 @@ import { StatusCodes } from "http-status-codes";
 import { Model } from "../Model";
 import { TaskTagsRepositorySQLite } from "@models/repositories/sqlite/TaskTagsRepositorySQLite";
 import { UserTasksRepositorySQLite } from "@models/repositories/sqlite/UserTasksRepositorySQLite";
+import { User } from "@models/User";
+import { Task } from "@models/Task";
+import { Tag } from "@models/Tag";
 
-export const get_tags = (req, res) => {
+function checkReqThrowIfInvalid(req: { params: { taskId: any } }) {
+  const taskId = req.params.taskId;
+  if (typeof taskId !== "string") {
+    throw createError(StatusCodes.BAD_REQUEST,
+      `Invalid type: taskId with value ${taskId} is of type ${typeof taskId} instead of string`);
+  }
+  if (!Task.isValidId(taskId)) throw createError(StatusCodes.BAD_REQUEST, `Invalid ID = ${taskId}`);
+}
+
+export const get_tags = (req: { user?: any; params: { taskId: string; }; }, res) => {
+  checkReqThrowIfInvalid(req);
   const userTasksRepository = new UserTasksRepositorySQLite(Model.db, req.user);
   const task = userTasksRepository.read(req.params.taskId);
   if (task == null) throw createError(StatusCodes.NOT_FOUND, "Task not found");
@@ -12,10 +25,11 @@ export const get_tags = (req, res) => {
   res.send(new TaskTagsRepositorySQLite(Model.db, task).readAll());
 };
 
-export const get_tag = (req, res) => {
+export const get_tag = (req: { user: User; params: { taskId: string; }; }, res) => {
+  checkReqThrowIfInvalid(req);
   const userTasksRepository = new UserTasksRepositorySQLite(Model.db, req.user);
   const task = userTasksRepository.read(req.params.taskId);
-  if (task == null) throw createError(StatusCodes.NOT_FOUND, "Task not found");
+  if (task == null) throw createError(StatusCodes.NOT_FOUND, `Task not found. Task ID = ${req.params.taskId}`);
 
   res.send(new TaskTagsRepositorySQLite(Model.db, task).read(req.params.taskId));
 };
